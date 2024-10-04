d5589712cb745617292835ad38f50913d04b6aeb
Blanco237 <asongrandy9@gmail.com>
2024-10-04 14:18:40 +0100

    Boilerplate

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
-const express = require('express');
+const express = require("express");
+const mathRouter = require('./routes/math.route');
 
 const app = express();
 
-app.get('/', function (req, res) {
-    console.log("Request 2");
-    res.send("Failure")
+app.use(express.json());
+app.use('/math', mathRouter);
+
+app.get("/", function (req, res) {
+  console.log("Request 2");
+  res.send("Failure");
+});
+
+app.get('/user/:username', (req, res) => {
+    const params = req.params;
+    console.log(params);
+    res.send(params.username)
 })
 
-app.get('/hello', function (req, res ) {
-    console.log("Hello World");
-    res.sendStatus(201);
-})
+app.get("/hello", function (req, res) {
+  console.log("Hello World");
+  res.sendStatus(201);
+});
 
-app.get('/data', function (req, res ) {
-    res.json([9,38,329,49848])
-})
+app.get("/data", function (req, res) {
+  res.json([9, 38, 329, 49848]);
+});
 
-app.post('/new', function () {
+app.post("/new", function (req, res) {
+  const incoming = req.body;
+  console.log("Recieved ", incoming);
+  res.sendStatus(200);
+});
 
-})
+
 
 const PORT = 5500;
 
-app.listen(PORT,function () {
-    console.log(`Server is running on port: ${PORT}`);
-})
+app.listen(PORT, function () {
+  console.log(`Server is running on port: ${PORT}`);
+});
